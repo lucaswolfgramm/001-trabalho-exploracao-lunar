3811c9e7141acd4b4635decdfda35458f4fa00cf
Lucas Wolfgramm <lucas.wol@hotmail.com>
2018-04-13 19:42:59 -0300

    .

---
+++ graph/test/graph/Q2.java
@@ -0,0 +1,34 @@
+package graph;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Q2 {
+
+	@Test
+	public void hasPathTest() {
+		Graph<String> graph = new Graph<>();
+		Vertex<String> d = graph.addVertex("d");
+		Vertex<String> g = graph.addVertex("g");
+		Vertex<String> f = graph.addVertex("f");
+		Vertex<String> r = graph.addVertex("r");
+		Vertex<String> o = graph.addVertex("o");
+		Vertex<String> m = graph.addVertex("m");
+		Vertex<String> l = graph.addVertex("l");
+
+		d.addEdge(g, 100);
+		d.addEdge(f, 10);
+		f.addEdge(o, 15);
+		f.addEdge(m, 25);
+		f.addEdge(r, 75);
+		g.addEdge(r, 30);
+		o.addEdge(m, 55);
+		m.addEdge(l, 40);
+		l.addEdge(r, 5);
+
+		boolean result = graph.hasPath(f, g);
+		Assert.assertFalse(result);
+
+	}
+
+}
